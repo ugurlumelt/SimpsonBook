149538f1e2253449e40fded09f409f65fe5159c9
Meltem Ugurlu <ugurlumeltem7@icloud.com>
2023-05-08 21:13:40 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Meltem Uğurlu on 4.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var mySimpsons = [Simpson]()
+    var chosenSimpson : Simpson?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let homer = Simpson(name: "Homer Simpson", job: "Nuclear Safety", image: UIImage(named: "homer")!)
+        let marge = Simpson(name: "Marge Simpson", job: "Housewife", image: UIImage(named: "marge")!)
+        let bart = Simpson(name: "Bart Simpson", job: "Student", image: UIImage(named: "bart")!)
+        let lisa = Simpson(name: "Lisa Simpson", job: "Student", image: UIImage(named: "lisa")!)
+        let maggie = Simpson(name: "Maggie Simpson", job: "Baby", image: UIImage(named: "maggie")!)
+        
+        mySimpsons.append(homer)
+        mySimpsons.append(marge)
+        mySimpsons.append(bart)
+        mySimpsons.append(lisa)
+        mySimpsons.append(maggie)
+    
+      
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mySimpsons.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = mySimpsons[indexPath.row].name
+        cell.contentConfiguration = content
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chosenSimpson = mySimpsons[indexPath.row]
+        self.performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC"{
+            let destinationVC = segue.destination as! detailsVC
+            destinationVC.selectedSimpson = chosenSimpson
+        }
+    }
+}
+
---
+++ SimpsonBook/detailsVC.swift
@@ -0,0 +1,33 @@
+//
+//  detailsVC.swift
+//  SimpsonBook
+//
+//  Created by Meltem Uğurlu on 4.05.2023.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    var selectedSimpson : Simpson?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameLabel.text = selectedSimpson?.name
+        jobLabel.text = selectedSimpson?.job
+        imageView.image = selectedSimpson?.image
+       
+    }
+    
+
+  
+
+}
